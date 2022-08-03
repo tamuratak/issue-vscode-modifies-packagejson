db2b097b0344518a573c84472166fa5eb17ac3fc
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-04 08:54:13 +0900

    edit

--- test/unittest.index.ts
+++ test/unittest.index.ts
@@ -12,7 +12,7 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '.')
 
     return new Promise((resolve, reject) => {
-        glob('unittests/**/*.test.js', { cwd: testsRoot }, (err, files) => {
+        glob('*.test.js', { cwd: testsRoot }, (err, files) => {
             if (err) {
                 return reject(err)
             }
