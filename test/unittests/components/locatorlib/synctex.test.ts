f8a8905573322277671b3a025ec2f1abd0ef59bf
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-04 08:51:57 +0900

    edit

--- test/unittests/components/locatorlib/synctex.test.ts
+++ test/unittests/components/locatorlib/synctex.test.ts
@@ -1,38 +1,16 @@
 import * as assert from 'assert'
-import * as path from 'path'
-import * as vscode from 'vscode'
 
 export function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function getWorkspaceRootDir(): string | undefined {
-    let rootDir: string | undefined
-    if (vscode.workspace.workspaceFile) {
-        rootDir = path.dirname(vscode.workspace.workspaceFile.fsPath)
-    } else {
-        rootDir = vscode.workspace.workspaceFolders?.[0].uri.fsPath
-    }
-    return rootDir
-}
-
-/**
- * Runs `cb` as a test if the basename of the working directory is equal to `fixtureName`.
- *
- * @param fixtureName The name of a fixture directory of the current test.
- * @param label Used as the title of test.
- * @param cb Callback executing tests.
- * @param skip `cb` is skipped if `true` returned.
- */
  export function runUnitTestWithFixture(
     fixtureName: string,
     label: string,
     cb: () => unknown,
-    skip?: () => boolean
+    _skip?: () => boolean
 ) {
-    const rootPath = getWorkspaceRootDir()
-    const shouldSkip = skip && skip()
-    if (rootPath && path.basename(rootPath) === fixtureName && !shouldSkip) {
+
         test( fixtureName + ': ' + label, async () => {
             try {
                 await cb()
@@ -41,7 +19,7 @@ function getWorkspaceRootDir(): string | undefined {
                 throw e
             }
         })
-    }
+//    }
 }
 
 suite('unit test suite', () => {
@@ -49,8 +27,13 @@ suite('unit test suite', () => {
     suiteSetup(() => {
     })
 
-    runUnitTestWithFixture('fixture001', 'test synctex', () => {
-        assert.ok(false)
+    test( 'fixture001', async () => {
+        try {
+            assert.ok(false)
+        } catch (e) {
+            await sleep(1000)
+            throw e
+        }
     })
 
 })
