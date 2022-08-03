66226f9c97bb8ae985dda40f1c703deff67e9365
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-04 08:52:38 +0900

    edit

--- test/unittests/components/locatorlib/synctex.test.ts
+++ test/unittests/components/locatorlib/synctex.test.ts
@@ -4,30 +4,9 @@ export function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
- export function runUnitTestWithFixture(
-    fixtureName: string,
-    label: string,
-    cb: () => unknown,
-    _skip?: () => boolean
-) {
-
-        test( fixtureName + ': ' + label, async () => {
-            try {
-                await cb()
-            } catch (e) {
-                await sleep(1000)
-                throw e
-            }
-        })
-//    }
-}
-
 suite('unit test suite', () => {
 
-    suiteSetup(() => {
-    })
-
-    test( 'fixture001', async () => {
+    test('fixture001', async () => {
         try {
             assert.ok(false)
         } catch (e) {
