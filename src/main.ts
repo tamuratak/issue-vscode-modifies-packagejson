6d8030b4b4db1c89e0438c6191575bc0e798ed8d
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-04 08:57:57 +0900

    edit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 
 export function activate() {
-    //    const extension = new Extension()
-    //    extensionToDispose = extension
-    //    return generateLatexWorkshopApi(extension)
-          return undefined as any
-    }
+
+}
